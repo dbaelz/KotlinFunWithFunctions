ca90b377dce60b65d74a1f62594539c9482bf703
Daniel Bälz <me@dbaelz.de>
2018-02-13 21:43:47 +0100

    Initial commit with auto-generated project

---
+++ app/src/main/java/de/dbaelz/kug/kotlindemo/TopicActivity.kt
@@ -0,0 +1,12 @@
+package de.dbaelz.kug.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topic)
+    }
+}
