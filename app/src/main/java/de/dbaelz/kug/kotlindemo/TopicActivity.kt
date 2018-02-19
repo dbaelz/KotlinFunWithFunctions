575629d16bb687f11ae7a8c4cb18bf2338bf50d9
Daniel Bälz <me@dbaelz.de>
2018-02-19 18:17:44 +0100

    Minor improvement in activity view

--- app/src/main/java/de/dbaelz/kug/kotlindemo/TopicActivity.kt
+++ app/src/main/java/de/dbaelz/kug/kotlindemo/TopicActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.view.View
 import android.widget.TextView
 import de.dbaelz.kug.kotlindemo.data.EventService
@@ -15,37 +16,39 @@ class TopicActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic)
 
-        val titleText = findViewById<TextView>(R.id.event_title)
-        val locationText = findViewById<TextView>(R.id.event_location)
-        val slot1Text = findViewById<TextView>(R.id.event_slot1)
-        val slot2Text = findViewById<TextView>(R.id.event_slot2)
+        val titleTextView = findViewById<TextView>(R.id.event_title)
+        val locationTextView = findViewById<TextView>(R.id.event_location)
+        val slot1TextView = findViewById<TextView>(R.id.event_slot1)
+        val slot2TextView = findViewById<TextView>(R.id.event_slot2)
 
 
         val (info, topics, location) = EventService.getFirstEvent()
 
 
         if (info?.title != null) {
-            titleText.text = info.title
+            titleTextView.text = info.title
             if (info.link != null) {
-                titleText.setTextColor(resources.getColor(R.color.colorPrimaryDark))
-                titleText.setOnClickListener({
+                titleTextView.setTextColor(resources.getColor(R.color.colorPrimaryDark))
+                titleTextView.setOnClickListener({
                     startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(info.link)))
                 })
             }
         }
 
         if (location != null) {
-            locationText.text = getString(R.string.event_location_text, location.name, location.address,
+            locationTextView.text = getString(R.string.event_location_text, location.name, location.address,
                     location.zip, location.city)
-            locationText.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            locationTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
         }
 
         if (topics.size >= 2) {
             val slot1Topic = topics[0]
-            slot1Text.text = getString(R.string.event_slot_text, slot1Topic.author, slot1Topic.title)
+            slot1TextView.text = getString(R.string.event_slot_text, slot1Topic.author, slot1Topic.title)
+            slot1TextView.ellipsize = TextUtils.TruncateAt.END
 
             val slot2Topic = topics[1]
-            slot2Text.text = getString(R.string.event_slot_text, slot2Topic.author, slot2Topic.title)
+            slot2TextView.text = getString(R.string.event_slot_text, slot2Topic.author, slot2Topic.title)
+            slot2TextView.ellipsize = TextUtils.TruncateAt.END
         }
     }
 }
